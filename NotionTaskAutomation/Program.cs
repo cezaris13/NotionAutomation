e883327ff54444987ae422d66f1ca83a0cfe03f2
Pijus <pijus.petkevicius@bentley.com>
2024-07-22 22:39:59 +0300

    make swagger work

--- NotionTaskAutomation/Program.cs
+++ NotionTaskAutomation/Program.cs
@@ -15,16 +15,14 @@ class Program
     static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();
-        
-        builder.Services.AddControllers(); 
         builder.Services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "NotionAutomation", Version = "v1" });
         });
         builder.Services.AddMvcCore();
         builder.Services.AddMvc();
-
+        builder.Services.AddHttpClient();
+        
         var configurationBuilder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false);
@@ -35,12 +33,11 @@ class Program
         builder.Services.AddSingleton<IFilterFactory, FilterFactory>();
         var host = builder.Build();
         host.UseSwagger();
-        host.UseSwaggerUI();
         host.UseSwaggerUI(options =>
         {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-            options.RoutePrefix = String.Empty;
+            options.SwaggerEndpoint("v1/swagger.json", "v1");
         });
+        host.MapControllers();
         // await ExecuteTask(host.Services);
 
         await host.RunAsync();
