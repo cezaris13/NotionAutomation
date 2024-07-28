436bb4a133152dd5d318f8e5cdf038e4bebd9817
Pijus <pijus.petkevicius@bentley.com>
2024-07-28 16:27:49 +0300

    program cs push

--- NotionTaskAutomation/Program.cs
+++ NotionTaskAutomation/Program.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.IO;
-using System.Net.Mime;
+﻿using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 namespace NotionTaskAutomation;
@@ -30,7 +27,6 @@ class Program
         // services.AddHttpClient();
         builder.Services.AddSingleton(_ => configuration);
         builder.Services.AddSingleton<INotionButtonClicker, NotionButtonClicker>();
-        builder.Services.AddSingleton<IFilterFactory, FilterFactory>();
         var host = builder.Build();
         host.UseSwagger();
         host.UseSwaggerUI(options =>
@@ -38,16 +34,7 @@ class Program
             options.SwaggerEndpoint("v1/swagger.json", "v1");
         });
         host.MapControllers();
-        // await ExecuteTask(host.Services);
 
         await host.RunAsync();
     }
-
-    // private static async Task ExecuteTask(IServiceProvider hostProvider)
-    // {
-    //     var serviceScope = hostProvider.CreateScope();
-    //     var serviceProvider = serviceScope.ServiceProvider;
-    //     var notionButtonClicker = serviceProvider.GetService<INotionButtonClicker>();
-    //     await notionButtonClicker.ExecuteClickAsync();
-    // }
 }
